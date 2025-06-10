fdc38f5ab8a12489a00c913b95e56a737b6407d7
alanjohnck <alanjohnchacko.live@gmail.com>
2025-06-10 23:37:10 +0530

    starting folders

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
-
+import HeroSection from "./hero/page";
+//place all pages here
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-         <h1>OOFDI</h1>
-    </div>
+    <>
+         <HeroSection />
+         <div className="w-screen h-screen"></div>
+    </>
   );
 }
