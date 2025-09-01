aff5fdc798ac6ddc97cf689543759cca8198fe0c
Devou <devou.in@gmail.com>
2025-09-01 20:54:36 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -68,7 +68,7 @@ export default function RootLayout({ children }) {
         {/* Preconnect to Google Fonts to reduce render delay */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-
+        
         {/* Preload LCP image */}
         <link
           rel="preload"
@@ -80,6 +80,7 @@ export default function RootLayout({ children }) {
 
         {/*  Canonical tag */}
         <link rel="canonical" href="https://oofdi.vercel.app/" />
+        <meta name="google-site-verification" content="3AFKgVsoOY1ztrXlpfCK6QUuJhGVlPkXhVa3QfU6ClQ" />
       </head>
       <body className={`${monoSans.variable} overflow-x-hidden`}>
         <FirestoreProvider>
